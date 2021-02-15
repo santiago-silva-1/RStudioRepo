7c05f89865f1cab2498ba09de2176920b07a879e
Santiago Silva <santiago_silva@baylor.edu>
2021-02-15 11:49:34 -0600

    Finished with part 3 of the dataset and exported it

--- Code/MigrationRecordsScraping.R
+++ Code/MigrationRecordsScraping.R
@@ -77,21 +77,22 @@ firstnextpage$clickElement()
 ####build loop
 
 library(RSelenium)
-rD=RSelenium::rsDriver(port = 8957L, browser =("firefox"))
+rD=RSelenium::rsDriver(port = 2930L, browser =("firefox"))
 #that starts an remote Rselenium driver
 #a firefox window should open
 #need Java on the computer
 #might need to change port number
 remDr=rD$client
 
+remDr$navigate("https://aad.archives.gov/aad/display-partial-records.jsp?dt=180&sc=17169%2C17170%2C17172%2C17189%2C17177%2C17180%2C17190%2C17181&cat=all&tf=F&bc=sl%2Cfd&q=&as_alq=&as_anq=&as_epq=&as_woq=&nfo_17169=V%2C20%2C1900&op_17169=0&txt_17169=&nfo_17170=V%2C19%2C1900&op_17170=0&txt_17170=&nfo_17172=N%2C3%2C1900&cl_17172=&nfo_17189=N%2C3%2C1900&cl_17189=&nfo_17177=V%2C20%2C1900&op_17177=0&txt_17177=&nfo_17180=N%2C3%2C1900&cl_17180=&nfo_17190=N%2C8%2C1900&cl_17190=&nfo_17181=D%2C10%2C1846&op_17181=8&txt_17181=1%2F12%2F1846&txt_17181=12%2F31%2F1851&rpp=50&mtch=604596&pg=2803&rpp=50")
 
 #figure out how the page changes, 12092 pages total
 #IrishFamine3=data.frame()
-for (page_result in seq(from =2236, to= 2400, by=1)) {
+for (page_result in seq(from =3418, to= 3600, by=1)) {
  link= paste0("https://aad.archives.gov/aad/display-partial-records.jsp?dt=180&sc=17169%2C17170%2C17172%2C17189%2C17177%2C17180%2C17190%2C17181&cat=all&tf=F&bc=sl%2Cfd&q=&as_alq=&as_anq=&as_epq=&as_woq=&nfo_17169=V%2C20%2C1900&op_17169=0&txt_17169=&nfo_17170=V%2C19%2C1900&op_17170=0&txt_17170=&nfo_17172=N%2C3%2C1900&cl_17172=&nfo_17189=N%2C3%2C1900&cl_17189=&nfo_17177=V%2C20%2C1900&op_17177=0&txt_17177=&nfo_17180=N%2C3%2C1900&cl_17180=&nfo_17190=N%2C8%2C1900&cl_17190=&nfo_17181=D%2C10%2C1846&op_17181=8&txt_17181=1%2F12%2F1846&txt_17181=12%2F31%2F1851&rpp=50&mtch=604596&pg=", page_result ,"&rpp=50")
  remDr$navigate(link)
  
-Sys.sleep(5)
+Sys.sleep(3)
 
 lastname=remDr$findElements("css selector", "tbody td:nth-child(2)")
 LastName=unlist(lapply(lastname, function(x){x$getElementText()}))
@@ -126,20 +127,21 @@ arrivaldate=remDr$findElements("css selector", "tbody td:nth-child(9)")
 ArrivalDate=unlist(lapply(arrivaldate, function(x){x$getElementText()}))
 
 
-IrishFamine2=rbind(IrishFamine2 , data.frame(LastName=LastName,FirstName=FirstName , Age= Age, NativeCountryCode= NativeCtryCode,
+IrishFamine3=rbind(IrishFamine3 , data.frame(LastName=LastName,FirstName=FirstName , Age= Age, NativeCountryCode= NativeCtryCode,
                                        Destination= Destination, PassengerPortOfEmbarkationCode= PortofEmbark, 
                                        ManifestIdentificationNumber=ManifestNum , 
                                        PassengerArrivalDate=ArrivalDate ,stringsAsFactors = FALSE))
-print(paste("Page:", page_result))
+print(paste("Page:", page_result, print(dim(IrishFamine3))))
 }
-dim(IrishFamine1)
-dim(IrishFamine2)
+dim(IrishFamine3)
+tail(IrishFamine3)
 #sum(dim(IrishFamine1)-dim(IrishFamine2))
 #missing 50 obs from IrishFamine1
 #missing 900 observations from IrishFamine2
 
 #IrishFamine1 goes from page 1 to 1200
 #IrishFamine2 goes from page 1201 to 2400, start time 1:00 pm
+##IrishFamine2 goes from page 2401 to 3600, lost 150 obs
 
 
 #need to add last page manually because it has 20 observations, not 50 like the other pages
@@ -191,8 +193,8 @@ remDr$close()
 #export
 
 write.xlsx(IrishFamine1, "PeopleWhoMigrated.xlsx")
-write.xlsx2(IrishFamine2, "PeopleWhoMigratedpart2.xlsx")
-openxlsx::write.xlsx(IrishFamine2, file = "PeopleWhoMigratedpart2.xlsx")
+write.xlsx2(IrishFamine3, "PeopleWhoMigratedpart3.xlsx")
+openxlsx::write.xlsx(IrishFamine3, file = "PeopleWhoMigratedpart3.xlsx")
 
 ?write
 
