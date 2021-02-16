c3f685b8547dce449df7466e97a6e7cfe2c1feac
Santiago Silva <santiago_silva@baylor.edu>
2021-02-15 18:30:42 -0600

    Finished the 4th part of the dataset, and saved it

--- Code/MigrationRecordsScraping.R
+++ Code/MigrationRecordsScraping.R
@@ -87,8 +87,8 @@ remDr=rD$client
 remDr$navigate("https://aad.archives.gov/aad/display-partial-records.jsp?dt=180&sc=17169%2C17170%2C17172%2C17189%2C17177%2C17180%2C17190%2C17181&cat=all&tf=F&bc=sl%2Cfd&q=&as_alq=&as_anq=&as_epq=&as_woq=&nfo_17169=V%2C20%2C1900&op_17169=0&txt_17169=&nfo_17170=V%2C19%2C1900&op_17170=0&txt_17170=&nfo_17172=N%2C3%2C1900&cl_17172=&nfo_17189=N%2C3%2C1900&cl_17189=&nfo_17177=V%2C20%2C1900&op_17177=0&txt_17177=&nfo_17180=N%2C3%2C1900&cl_17180=&nfo_17190=N%2C8%2C1900&cl_17190=&nfo_17181=D%2C10%2C1846&op_17181=8&txt_17181=1%2F12%2F1846&txt_17181=12%2F31%2F1851&rpp=50&mtch=604596&pg=2803&rpp=50")
 
 #figure out how the page changes, 12092 pages total
-#IrishFamine3=data.frame()
-for (page_result in seq(from =3418, to= 3600, by=1)) {
+#IrishFamine4=data.frame()
+for (page_result in seq(from =4797, to= 4800, by=1)) {
  link= paste0("https://aad.archives.gov/aad/display-partial-records.jsp?dt=180&sc=17169%2C17170%2C17172%2C17189%2C17177%2C17180%2C17190%2C17181&cat=all&tf=F&bc=sl%2Cfd&q=&as_alq=&as_anq=&as_epq=&as_woq=&nfo_17169=V%2C20%2C1900&op_17169=0&txt_17169=&nfo_17170=V%2C19%2C1900&op_17170=0&txt_17170=&nfo_17172=N%2C3%2C1900&cl_17172=&nfo_17189=N%2C3%2C1900&cl_17189=&nfo_17177=V%2C20%2C1900&op_17177=0&txt_17177=&nfo_17180=N%2C3%2C1900&cl_17180=&nfo_17190=N%2C8%2C1900&cl_17190=&nfo_17181=D%2C10%2C1846&op_17181=8&txt_17181=1%2F12%2F1846&txt_17181=12%2F31%2F1851&rpp=50&mtch=604596&pg=", page_result ,"&rpp=50")
  remDr$navigate(link)
  
@@ -127,21 +127,21 @@ arrivaldate=remDr$findElements("css selector", "tbody td:nth-child(9)")
 ArrivalDate=unlist(lapply(arrivaldate, function(x){x$getElementText()}))
 
 
-IrishFamine3=rbind(IrishFamine3 , data.frame(LastName=LastName,FirstName=FirstName , Age= Age, NativeCountryCode= NativeCtryCode,
+IrishFamine4=rbind(IrishFamine4 , data.frame(LastName=LastName,FirstName=FirstName , Age= Age, NativeCountryCode= NativeCtryCode,
                                        Destination= Destination, PassengerPortOfEmbarkationCode= PortofEmbark, 
                                        ManifestIdentificationNumber=ManifestNum , 
                                        PassengerArrivalDate=ArrivalDate ,stringsAsFactors = FALSE))
 print(paste("Page:", page_result, print(dim(IrishFamine3))))
 }
-dim(IrishFamine3)
-tail(IrishFamine3)
+dim(IrishFamine4)
+tail(IrishFamine4)
 #sum(dim(IrishFamine1)-dim(IrishFamine2))
 #missing 50 obs from IrishFamine1
 #missing 900 observations from IrishFamine2
 
 #IrishFamine1 goes from page 1 to 1200
 #IrishFamine2 goes from page 1201 to 2400, start time 1:00 pm
-##IrishFamine2 goes from page 2401 to 3600, lost 150 obs
+##IrishFamine2 goes from page 2401 to 3600,
 
 
 #need to add last page manually because it has 20 observations, not 50 like the other pages
@@ -194,7 +194,7 @@ remDr$close()
 
 write.xlsx(IrishFamine1, "PeopleWhoMigrated.xlsx")
 write.xlsx2(IrishFamine3, "PeopleWhoMigratedpart3.xlsx")
-openxlsx::write.xlsx(IrishFamine3, file = "PeopleWhoMigratedpart3.xlsx")
+openxlsx::write.xlsx(IrishFamine4, file = "PeopleWhoMigratedpart4.xlsx")
 
 ?write
 
