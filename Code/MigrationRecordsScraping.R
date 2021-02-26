8e8f27b3e489195eba6bf9b03a53c4b79beafe8f
Santiago Silva <santiago_silva@baylor.edu>
2021-02-26 17:59:32 -0600

    finished and saved 9th part of the dataset

--- Code/MigrationRecordsScraping.R
+++ Code/MigrationRecordsScraping.R
@@ -80,22 +80,22 @@ firstnextpage$clickElement()
 ####build loop
 
 library(RSelenium)
-rD=RSelenium::rsDriver(port = 8011L, browser =("firefox"))
+rD=RSelenium::rsDriver(port = 7011L, browser =("firefox"))
 #that starts an remote Rselenium driver
 #a firefox window should open
 #need Java on the computer
 #might need to change port number
 remDr=rD$client
 
-remDr$navigate("https://aad.archives.gov/aad/display-partial-records.jsp?dt=180&sc=17169%2C17170%2C17172%2C17189%2C17177%2C17180%2C17190%2C17181&cat=all&tf=F&bc=sl%2Cfd&q=&as_alq=&as_anq=&as_epq=&as_woq=&nfo_17169=V%2C20%2C1900&op_17169=0&txt_17169=&nfo_17170=V%2C19%2C1900&op_17170=0&txt_17170=&nfo_17172=N%2C3%2C1900&cl_17172=&nfo_17189=N%2C3%2C1900&cl_17189=&nfo_17177=V%2C20%2C1900&op_17177=0&txt_17177=&nfo_17180=N%2C3%2C1900&cl_17180=&nfo_17190=N%2C8%2C1900&cl_17190=&nfo_17181=D%2C10%2C1846&op_17181=8&txt_17181=1%2F12%2F1846&txt_17181=12%2F31%2F1851&rpp=50&mtch=604596&pg=7200&rpp=50")
+remDr$navigate("https://aad.archives.gov/aad/display-partial-records.jsp?dt=180&sc=17169%2C17170%2C17172%2C17189%2C17177%2C17180%2C17190%2C17181&cat=all&tf=F&bc=sl%2Cfd&q=&as_alq=&as_anq=&as_epq=&as_woq=&nfo_17169=V%2C20%2C1900&op_17169=0&txt_17169=&nfo_17170=V%2C19%2C1900&op_17170=0&txt_17170=&nfo_17172=N%2C3%2C1900&cl_17172=&nfo_17189=N%2C3%2C1900&cl_17189=&nfo_17177=V%2C20%2C1900&op_17177=0&txt_17177=&nfo_17180=N%2C3%2C1900&cl_17180=&nfo_17190=N%2C8%2C1900&cl_17190=&nfo_17181=D%2C10%2C1846&op_17181=8&txt_17181=1%2F12%2F1846&txt_17181=12%2F31%2F1851&rpp=50&mtch=604596&pg=9600&rpp=50")
 
 #figure out how the page changes, 12092 pages total
-#IrishFamine8=data.frame()
-for (page_result in seq(from =8990 , to= 9600, by=1)) {
+#IrishFamine10=data.frame()
+for (page_result in seq(from =10801 , to= 12092, by=1)) {
  link= paste0("https://aad.archives.gov/aad/display-partial-records.jsp?dt=180&sc=17169%2C17170%2C17172%2C17189%2C17177%2C17180%2C17190%2C17181&cat=all&tf=F&bc=sl%2Cfd&q=&as_alq=&as_anq=&as_epq=&as_woq=&nfo_17169=V%2C20%2C1900&op_17169=0&txt_17169=&nfo_17170=V%2C19%2C1900&op_17170=0&txt_17170=&nfo_17172=N%2C3%2C1900&cl_17172=&nfo_17189=N%2C3%2C1900&cl_17189=&nfo_17177=V%2C20%2C1900&op_17177=0&txt_17177=&nfo_17180=N%2C3%2C1900&cl_17180=&nfo_17190=N%2C8%2C1900&cl_17190=&nfo_17181=D%2C10%2C1846&op_17181=8&txt_17181=1%2F12%2F1846&txt_17181=12%2F31%2F1851&rpp=50&mtch=604596&pg=", page_result ,"&rpp=50")
  remDr$navigate(link)
  
-Sys.sleep(4.2)
+Sys.sleep(3.5)
 
 lastname=remDr$findElements("css selector", "tbody td:nth-child(2)")
 LastName=unlist(lapply(lastname, function(x){x$getElementText()}))
@@ -130,14 +130,14 @@ arrivaldate=remDr$findElements("css selector", "tbody td:nth-child(9)")
 ArrivalDate=unlist(lapply(arrivaldate, function(x){x$getElementText()}))
 
 
-IrishFamine8=rbind(IrishFamine8 , data.frame(LastName=LastName,FirstName=FirstName , Age= Age, NativeCountryCode= NativeCtryCode,
+IrishFamine10=rbind(IrishFamine10 , data.frame(LastName=LastName,FirstName=FirstName , Age= Age, NativeCountryCode= NativeCtryCode,
                                        Destination= Destination, PassengerPortOfEmbarkationCode= PortofEmbark, 
                                        ManifestIdentificationNumber=ManifestNum , 
                                        PassengerArrivalDate=ArrivalDate ,stringsAsFactors = FALSE))
-print(paste("Page:", page_result, (dim(IrishFamine8))))
+print(paste("Page:", page_result, (dim(IrishFamine10))))
 }
-dim(IrishFamine8)
-tail(IrishFamine8)
+dim(IrishFamine10)
+tail(IrishFamine10)
 #sum(dim(IrishFamine1)-dim(IrishFamine2))
 #missing 50 obs from IrishFamine1
 #missing 900 observations from IrishFamine2
@@ -145,6 +145,7 @@ tail(IrishFamine8)
 #missing 1800 obs from Irishfamine6
 #extra 9200 obs in irishfamine 7, all picked up before page 7754
 #missing 250 obs in irishfamine 8
+#missing 50 ob sin irishfamine9
 
 #IrishFamine1 goes from page 1 to 1200
 #IrishFamine2 goes from page 1201 to 2400, 
@@ -154,7 +155,8 @@ tail(IrishFamine8)
 #IrishFamine6 goes from page 6001 to 7200,
 #IrishFamine7 goes from page 7201 to 8400,
 #IrishFamine8 goes from page 8401 to 9600,
-
+#IrishFamine9 goes from page 9601 to 10,800,
+#IrishFamine10 goes from page 10,801 to 12,092,
 
 
 
@@ -208,7 +210,7 @@ remDr$close()
 
 write.xlsx(IrishFamine1, "PeopleWhoMigrated.xlsx")
 write.xlsx2(IrishFamine3, "PeopleWhoMigratedpart3.xlsx")
-openxlsx::write.xlsx(IrishFamine8, file = "PeopleWhoMigratedpart8.xlsx")
+openxlsx::write.xlsx(IrishFamine9, file = "PeopleWhoMigratedpart9.xlsx")
 
 ?write
 
